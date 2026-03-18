b0f6ac9dd2fc9ecb1a02b1e15c5bc8dca25e0cd0
Mls <marceaulesech@gmail.com>
2026-03-18 13:52:30 +0100

    Update Attack.js

--- Fichiers/Attack.js
+++ Fichiers/Attack.js
@@ -5,10 +5,18 @@ import fast_moves from "./Info Pokémons/fast_moves.js";
 import charged_moves from "./Info Pokémons/charged_moves.js";
 
 class Attack {
-    constructor(nom, type, puissance, duree) {
+    constructor(nom, idAttaque, type, puissance, duree) {
         this.nom = nom;
+        this.idAttaque = idAttaque;
         this.type = type;
         this.puissance = puissance;
         this.duree = duree;
     }
+
+    toString() {
+        return this.nom + " : #" + this.idAttaque + ", " + this.type + ", " + this.puissance + ", " + this.duree + "ms";
+    }
 }
+
+
+console.log(new Attack("Tackle", 221, "Normal", 5, 500).toString());
