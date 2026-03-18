c436fec4bad2d3c9b1506e406178a9afce7471a5
Mls <marceaulesech@gmail.com>
2026-03-18 14:37:11 +0100

    Update Pokemon.js

--- Fichiers/Pokemon.js
+++ Fichiers/Pokemon.js
@@ -1,21 +1,36 @@
+import Type from "./Type.js";
+import Attack from "./Attack.js";
+
+import pokemon_moves from "./Info Pokémons/pokemon_moves.js";
+import pokemons from "./Info Pokémons/pokemons.js";
+
+import pokemon_types from "./Info Pokémons/pokemon_types.js";
+
 class Pokemon {
     static all_pokemons = {};
 
-    constructor(id, nom, stamina, base_attaque, base_defense, type, attaque_rapide, attaque_chargée) {
-        this.id = id;
+    constructor(nom) {
         this.nom = nom;
-        this.stamina = stamina;
-        this.base_attaque = base_attaque;
-        this.base_defense = base_defense;
-        this.types = new Type(type);
-        this.attaque_rapide = new Attack(attaque_rapide);
-        this.attaque_chargée = new Attack(attaque_chargée);
-        Pokemon.all_pokemons[id] = this;
+        const pokemonData = pokemons.find(p => p.pokemon_name === nom && p.form === "Normal") || pokemons.find(p => p.pokemon_name === nom);
+        const pokemonTypeData = pokemon_types.find(p => p.pokemon_name === nom && p.form === "Normal") || pokemon_types.find(p => p.pokemon_name === nom);
+        this.idPokemon = pokemonData.pokemon_id;
+        this.base_attaque = pokemonData.base_attack;
+        this.base_defense = pokemonData.base_defense;
+        this.stamina = pokemonData.base_stamina;
+        this.types = pokemonTypeData.type.map(type => Type.all_types[type]);
+        
+        const pokemonMovesData = pokemon_moves.find(p => p.pokemon_name === nom && p.form === "Normal") || pokemon_moves.find(p => p.pokemon_name === nom);
+        this.attacks = (pokemonMovesData ? (pokemonMovesData.charged_moves.concat(pokemonMovesData.fast_moves)) : []).map(attack => new Attack(attack));
+        
+        Pokemon.all_pokemons[this.idPokemon] = this;
     }
 
     toString() {
-        return this.nom + " : #" + this.id + ", [ " + this.type + "], [STA: " + this.stamina + " ATK: " + this.base_attaque + " DEF: " + this.base_defense + "], Rappide = [" + this.attaque_rapide + "], Chargée = [" + this.attaque_chargée + "]";
+        return this.nom + " : #" + this.idPokemon + ", " + this.base_attaque + ", " + this.base_defense + ", " + this.stamina;
     }
 }
 
-console.table(Pokemon.all_pokemons);
+//console.table(Pokemon.all_pokemons);
+
+
+console.log(new Pokemon("Bulbasaur").toString());
