ad0bb916e3307df20f96d3fb6e1ee133cb32c3c7
Mls <marceaulesech@gmail.com>
2026-03-18 15:02:00 +0100

    Update Pokemon.js

--- Fichiers/Pokemon.js
+++ Fichiers/Pokemon.js
@@ -11,27 +11,28 @@ class Pokemon {
 
     constructor(nom) {
 
-        const TEST = pokemons.find(p => p.pokemon_name == nom && p.form == "Normal");
-        const TYPE = pokemon_types.find(p => p.pokemon_name == nom && p.form == "Normal");
-        const MOVE = pokemon_moves.find(p => p.pokemon_name == nom && p.form == "Normal");
+        const lesInfosDuPokemon = pokemons.find(p => p.pokemon_name == nom && p.form == "Normal");
+        const lesTypesDuPokemon = pokemon_types.find(p => p.pokemon_name == nom && p.form == "Normal");
+        const lesAttauquesDuPokemon = pokemon_moves.find(p => p.pokemon_name == nom && p.form == "Normal");
         
         
 
         this.nom = nom;
-        this.form = TEST.form;
-        this.idPokemon = TEST.pokemon_id;
-        this.base_attaque = TEST.base_attack;
-        this.base_defense = TEST.base_defense;
-        this.stamina = TEST.base_stamina;
-        this.types = TYPE.type.map(type => Type.all_types[type]);
-        
-        this.attacks = (MOVE ? (MOVE.charged_moves.concat(MOVE.fast_moves)) : []).map(attack => new Attack(attack));
-        
+        this.idPokemon = lesInfosDuPokemon.pokemon_id;
+        this.base_attaque = lesInfosDuPokemon.base_attack;
+        this.base_defense = lesInfosDuPokemon.base_defense;
+        this.stamina = lesInfosDuPokemon.base_stamina;
+
+        this.types = lesTypesDuPokemon.type.map(type => Type.all_types[type]);
+
+        this.attaqueRapides = lesAttauquesDuPokemon.fast_moves;
+        this.attaqueChargees = lesAttauquesDuPokemon.charged_moves;
+
         Pokemon.all_pokemons[this.idPokemon] = this;
     }
 
     toString() {
-
+        return this.nom + " : #" + this.idPokemon + ", [Normal] , [STA: " + this.stamina + ", ATK: " + this.base_attaque + ", DEF: " + this.base_defense + "], Rapides = [" + this.attaqueRapides.join(", ") + "], Chargées = [" + this.attaqueChargees.join(", ") + "]";
     }
 }
 
